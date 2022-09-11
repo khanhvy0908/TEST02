2e034a2870bf5a0b1a97d9a36f6f4f9416334d38
khanhvy0908 <khanhvy0908>
2022-09-11 17:46:10 +0700

    test06

---
+++ main.js
@@ -0,0 +1,25 @@
+function student(name, age, address){
+    name: Vy
+    age: 18;
+    address: VN
+}
+console.log()
+
+
+
+//bai03
+const foo = (x, y, z) =>{
+    console.log(foo(x, y, z));
+};
+
+
+const foo = () =>{
+    alert('Hello');
+};
+console.log(foo());
+
+const foo (a, b){
+    let m = a+b*100;
+    return m;
+};
+console.log(foo());
